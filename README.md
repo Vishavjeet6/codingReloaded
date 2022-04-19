eeaf34600f905de0f12491bf5bf8f700063bc814
Vishavjeet Singh <vishavjeetsinghthakur@gmail.com>
2022-04-19 23:46:28 +0530

    angry p

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 * stack - https://youtube.com/playlist?list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd
 
 * binary-search - https://youtube.com/playlist?list=PL_z_8CaSLPWeYfhtuKHj-9MpYb6XQJ_f2
+
+* recursion - https://www.youtube.com/playlist?list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY
---
+++ coding/recursion/ReverseaStackusingRecursion9.java
@@ -0,0 +1,36 @@
+package coding.recursion;
+
+import java.util.Stack;
+
+public class ReverseaStackusingRecursion9 {
+	
+	public static void main(String[] args) {
+		Stack<Integer> st = new Stack<>();
+		st.push(1);
+		st.push(11);
+		st.push(22);
+		st.push(33);
+		st.push(44);
+		reverse(st);
+		st.forEach(System.out::println);
+	}
+
+	private static void reverse(Stack<Integer> st) {
+		if(st.size() == 1) return;
+		int temp = st.pop();
+		reverse(st);
+		insertAtStart(st, temp);
+	}
+
+	private static void insertAtStart(Stack<Integer> st, int temp) {
+		if(st.isEmpty()) {
+			st.push(temp);
+			return;
+		}
+		int val = st.pop();
+		insertAtStart(st, temp);
+		st.push(val);
+		return;
+	}
+
+}
