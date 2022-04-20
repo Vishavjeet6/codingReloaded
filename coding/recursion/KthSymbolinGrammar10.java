624f60140d346c8c25035bd41ba6cd1bc0d77690
Vishavjeet Singh <vishavjeetsinghthakur@gmail.com>
2022-04-20 23:55:23 +0530

    Hanoi hogya

---
+++ coding/recursion/KthSymbolinGrammar10.java
@@ -0,0 +1,22 @@
+package coding.recursion;
+
+
+//leetcode 779
+public class KthSymbolinGrammar10 {
+	
+	public static void main(String[] args) {
+		System.out.println(kthGrammar(2,2));
+	}
+	
+    public static int kthGrammar(int n, int k) {
+    	if(n==1 && k==1) return 0;
+    	
+    	int mid = (int) (Math.pow(2, n-1)/2);
+    	if(k <= mid) {
+    		return kthGrammar(n-1, k);
+    	}else {
+    		return kthGrammar(n-1, k-mid)^1;
+    	}
+    }
+
+}
